57c3b1ef63575397ed3a70c6ed563cc07926c338
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-07-22 22:59:46 +0200

    feat(client): create client

---
+++ client.js
@@ -0,0 +1,34 @@
+const { promisify } = require('util');
+const grpc = require('grpc');
+
+const DEFAULT_HOST = '127.0.0.1';
+const DEFAULT_PORT = 5000;
+
+module.exports = function client({host = DEFAULT_HOST, port = DEFAULT_PORT, encode, decode}, credentials) {
+    // XXX credentials + native options should be exposed as top-level arguments
+    const insecureCredentials = grpc.credentials.createInsecure();
+    const client = new grpc.Client(`${host}:${port}`, credentials || insecureCredentials);
+    let globalEncode, globalDecode;
+
+    const close = client.close.bind(client);
+    const waitForReady = promisify(client.waitForReady.bind(client));
+    const makeUnaryRequest = promisify(client.makeUnaryRequest.bind(client));
+
+    globalEncode = encode;
+    globalDecode = decode;
+
+    function ready(deadline) {
+        return waitForReady(Date.now() + deadline); // XXX Chainable API?
+    }
+
+    function call(method, request, metadata, encode, decode) {
+        // XXX Check that (encode || globalEncode) and (decode || globalDecode) are valid.
+        return makeUnaryRequest(method, encode || globalEncode, decode || globalDecode, request, metadata);
+    }
+
+    return {
+        ready,
+        call,
+        close
+    };
+};
